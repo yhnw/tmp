144deee21e66e46859b6dee37e5bfff0e7d8fdb4
yhnw <yhnwsk@gmail.com>
2025-06-05 12:46:55 +0900

    change

--- httpsession/httpsession.go
+++ httpsession/httpsession.go
@@ -81,14 +81,19 @@ func NewMiddleware[T any]() *Middleware[T] {
 	}
 }
 
-func (m *Middleware[T]) DeleteExpiredInterval(ctx context.Context, interval time.Duration) {
+func (m *Middleware[T]) DeleteExpiredInterval(ctx context.Context, interval time.Duration, errorHandler func(error)) {
+	if errorHandler == nil {
+		errorHandler = func(err error) {
+			slog.ErrorContext(ctx, "httpsession.DeleteExpiredInterval: "+err.Error())
+		}
+	}
 	cleanup := func() {
 		c := time.Tick(interval)
 		for {
 			select {
 			case <-c:
 				if err := m.Store.DeleteExpired(ctx); err != nil {
-					slog.ErrorContext(ctx, err.Error())
+					errorHandler(err)
 				}
 			case <-ctx.Done():
 				return
