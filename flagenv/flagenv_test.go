310bbd3fa6cbfae08a8fd0e56940a21101d9460f
yhnw <yhnwsk@gmail.com>
2025-08-20 15:54:51 +0900

    change

--- flagenv/flagenv_test.go
+++ flagenv/flagenv_test.go
@@ -31,351 +31,346 @@ func newFlagSet() (*flag.FlagSet, *flags) {
 	return fs, &flags
 }
 
+func run[T any](t *testing.T, fn func(*testing.T, T), name string, tc T) {
+	t.Helper()
+	t.Run(name, func(t *testing.T) {
+		t.Helper()
+		t.Log("@" + t.Name())
+		fn(t, tc)
+	})
+}
+
 func TestParse(t *testing.T) {
-	tests := []struct {
-		name      string
+	tempDir := t.TempDir()
+
+	type testCase struct {
 		args      []string
 		env       []string
 		config    string
 		envPrefix string
 		wantFlag  string
 		wantErr   string
-	}{
-		{
-			args:     []string{},
-			wantFlag: defaultFlags.accessKey,
-		},
-		{
-			args:     []string{"-access-key", "asdf"},
-			wantFlag: "asdf",
-		},
-		{
-			args:     []string{"--", "-access-key", "asdf"},
-			wantFlag: defaultFlags.accessKey,
-		},
-		{
-			env:      []string{"ACCESS_KEY", "env"},
-			wantFlag: "env",
-		},
-		{
-			args:     []string{"--"},
-			env:      []string{"ACCESS_KEY", "env"},
-			wantFlag: "env",
-		},
-		{
-			args:     []string{"-access-key", "asdf"},
-			env:      []string{"ACCESS_KEY", "env"},
-			wantFlag: "asdf",
-		},
-		{
-			args:     []string{"--", "-access-key", "asdf"},
-			env:      []string{"ACCESS_KEY", "env"},
-			wantFlag: "env",
-		},
-		{
-			envPrefix: "PREFIX_",
-			env:       []string{"ACCESS_KEY", "env"},
-			wantFlag:  defaultFlags.accessKey,
-		},
-		{
-			envPrefix: "PREFIX_",
-			env:       []string{"PREFIX_ACCESS_KEY", "env"},
-			wantFlag:  "env",
-		},
-		{
-			envPrefix: "PREFIX_",
-			args:      []string{"-access-key", "asdf"},
-			env:       []string{"PREFIX_ACCESS_KEY", "env"},
-			wantFlag:  "asdf",
-		},
-		{
-			config: `
-			# comment
-			-access-key 🔑 # comment
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			# comment
-			-access-key=🔑 # comment
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			ACCESS_KEY=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			UNDEF=🔑
-			`,
-			wantErr: "undefined",
-		},
-		{
-			envPrefix: "PREFIX_",
-			config: `
-			PREFIX_ACCESS_KEY=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			envPrefix: "PREFIX_",
-			config: `
-			ACCESS_KEY=🔑
-			ACCESS_KEY2=🔑
-			`,
-			wantErr: "undefined",
-		},
-		{
-			env: []string{"ACCESS_KEY", "env"},
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "env",
-		},
-		{
-			args: []string{"-access-key", "asdf"},
-			config: `
-			ACCESS_KEY=🔑
-			`,
-			wantFlag: "asdf",
-		},
-		{
-			config: `
-			-access-key=sss
-			ACCESS_KEY=🔑
-			`,
-			wantErr: "duplicate error",
-		},
-		{
-			config: `
-			ACCESS_KEY=🔑
-			-access-key=sss
-			`,
-			wantErr: "duplicate error",
-		},
-		{
-			config: `
-			-access-key=🔑
-			ACCESS_KEY
-			`,
-			wantErr: "missing =",
-		},
 	}
-	tempDir := t.TempDir()
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			fs, flags := newFlagSet()
-			if tt.config != "" {
-				f, err := os.CreateTemp(tempDir, "")
-				if err != nil {
-					t.Fatal(err)
-				}
-				defer f.Close()
-				_, err = io.Copy(f, strings.NewReader(tt.config))
-				if err != nil {
-					t.Fatal(err)
-				}
-				tt.args = append([]string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}, tt.args...)
-			}
-			for v := range slices.Chunk(tt.env, 2) {
-				t.Setenv(v[0], v[1])
-			}
-			err := Parse(fs, tt.args, tt.envPrefix)
-			if (err != nil) && (tt.wantErr != "") {
-				if !strings.Contains(err.Error(), tt.wantErr) {
-					t.Errorf("expected err contains %q, but got %q", tt.wantErr, err)
-				}
-				return
-			}
-			if (err == nil) && (tt.wantErr != "") {
-				t.Error("expected error but got nil")
-			}
-			if err != nil && (tt.wantErr == "") {
-				t.Error(err)
-			}
-			if tt.wantFlag != "" {
-				if g, w := flags.accessKey, tt.wantFlag; g != w {
-					t.Errorf("got %q, want %q", g, w)
-				}
-			}
-		})
-	}
-}
 
-func TestParseLoadFile(t *testing.T) {
-	tests := []struct {
-		name     string
-		config   string
-		wantFlag string
-		wantErr  string
-	}{
-		{
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			config: `
-			-access-key 🔑 extra
-			`,
-			wantErr: "syntax error",
-		},
-		{
-			config: `
-			ACCESS_KEY =🔑
-			`,
-			wantErr: "syntax error",
-		},
-		{
-			config: `
-			ACCESS KEY =🔑
-			`,
-			wantErr: "syntax error",
-		},
-	}
-	tempDir := t.TempDir()
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			fs, flags := newFlagSet()
-			var args []string
+	testFunc := func(t *testing.T, tc testCase) {
+		fs, flags := newFlagSet()
+		if tc.config != "" {
 			f, err := os.CreateTemp(tempDir, "")
 			if err != nil {
 				t.Fatal(err)
 			}
 			defer f.Close()
-			_, err = io.Copy(f, strings.NewReader(tt.config))
+			_, err = io.Copy(f, strings.NewReader(tc.config))
 			if err != nil {
 				t.Fatal(err)
 			}
-			args = []string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}
-			err = Parse(fs, args, "")
-			if (err != nil) && (tt.wantErr != "") {
-				if !strings.Contains(err.Error(), tt.wantErr) {
-					t.Errorf("expected err contains %q, but got %q", tt.wantErr, err)
-				}
-				return
+			tc.args = append([]string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}, tc.args...)
+		}
+		for v := range slices.Chunk(tc.env, 2) {
+			t.Setenv(v[0], v[1])
+		}
+		err := Parse(fs, tc.args, tc.envPrefix)
+		if (err != nil) && (tc.wantErr != "") {
+			if !strings.Contains(err.Error(), tc.wantErr) {
+				t.Errorf("expected err contains %q, but got %q", tc.wantErr, err)
 			}
-			if (err == nil) && (tt.wantErr != "") {
-				t.Error("expected error but got nil")
+			return
+		}
+		if (err == nil) && (tc.wantErr != "") {
+			t.Error("expected error but got nil")
+		}
+		if err != nil && (tc.wantErr == "") {
+			t.Error(err)
+		}
+		if tc.wantFlag != "" {
+			if g, w := flags.accessKey, tc.wantFlag; g != w {
+				t.Errorf("got %q, want %q", g, w)
 			}
-			if err != nil && (tt.wantErr == "") {
-				t.Error(err)
-			}
-			if tt.wantFlag != "" {
-				if g, w := flags.accessKey, tt.wantFlag; g != w {
-					t.Errorf("got %q, want %q", g, w)
-				}
-			}
-		})
+		}
 	}
+
+	run(t, testFunc, "", testCase{
+		args:     []string{},
+		wantFlag: defaultFlags.accessKey,
+	})
+	run(t, testFunc, "", testCase{
+		args:     []string{"-access-key", "asdf"},
+		wantFlag: "asdf",
+	})
+	run(t, testFunc, "", testCase{
+		args:     []string{"--", "-access-key", "asdf"},
+		wantFlag: defaultFlags.accessKey,
+	})
+	run(t, testFunc, "", testCase{
+		env:      []string{"ACCESS_KEY", "env"},
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		args:     []string{"--"},
+		env:      []string{"ACCESS_KEY", "env"},
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		args:     []string{"-access-key", "asdf"},
+		env:      []string{"ACCESS_KEY", "env"},
+		wantFlag: "asdf",
+	})
+	run(t, testFunc, "", testCase{
+		args:     []string{"--", "-access-key", "asdf"},
+		env:      []string{"ACCESS_KEY", "env"},
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		env:       []string{"ACCESS_KEY", "env"},
+		wantFlag:  defaultFlags.accessKey,
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		env:       []string{"PREFIX_ACCESS_KEY", "env"},
+		wantFlag:  "env",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		args:      []string{"-access-key", "asdf"},
+		env:       []string{"PREFIX_ACCESS_KEY", "env"},
+		wantFlag:  "asdf",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			# comment
+			-access-key 🔑 # comment
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			# comment
+			-access-key=🔑 # comment
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			ACCESS_KEY=🔑
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			UNDEF=🔑
+			`,
+		wantErr: "undefined",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		config: `
+			PREFIX_ACCESS_KEY=🔑
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		config: `
+			ACCESS_KEY=🔑
+			ACCESS_KEY2=🔑
+			`,
+		wantErr: "undefined",
+	})
+	run(t, testFunc, "", testCase{
+		env: []string{"ACCESS_KEY", "env"},
+		config: `
+			-access-key=🔑
+			`,
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		args: []string{"-access-key", "asdf"},
+		config: `
+			ACCESS_KEY=🔑
+			`,
+		wantFlag: "asdf",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			-access-key=sss
+			ACCESS_KEY=🔑
+			`,
+		wantErr: "duplicate error",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			ACCESS_KEY=🔑
+			-access-key=sss
+			`,
+		wantErr: "duplicate error",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			-access-key=🔑
+			ACCESS_KEY
+			`,
+		wantErr: "missing =",
+	})
+
 }
 
 func TestParseLoadFileFromEnv(t *testing.T) {
-	tests := []struct {
-		name      string
+	tempDir := t.TempDir()
+
+	type testCase struct {
 		config    string
 		configEnv string
 		envPrefix string
 		wantFlag  string
 		wantErr   string
-	}{
-		{
-			configEnv: `
-			-access-key=env
-			`,
-			wantFlag: "env",
-		},
-		{
-			envPrefix: "PREFIX_",
-			configEnv: `
-			-access-key=env
-			`,
-			wantFlag: "env",
-		},
-		{
-			configEnv: `
-			-access-key=env
-			`,
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "🔑",
-		},
-		{
-			envPrefix: "PREFIX_",
-			configEnv: `
-			-access-key=env
-			`,
-			config: `
-			-access-key=🔑
-			`,
-			wantFlag: "🔑",
-		},
 	}
+
+	testFunc := func(t *testing.T, tc testCase) {
+		fs, flags := newFlagSet()
+		var args []string
+		if tc.configEnv != "" {
+			f, err := os.CreateTemp(tempDir, "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+			_, err = io.Copy(f, strings.NewReader(tc.configEnv))
+			if err != nil {
+				t.Fatal(err)
+			}
+			t.Setenv(tc.envPrefix+strings.ToUpper(configFlagName), f.Name())
+		}
+		if tc.config != "" {
+			f, err := os.CreateTemp(tempDir, "")
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer f.Close()
+			_, err = io.Copy(f, strings.NewReader(tc.config))
+			if err != nil {
+				t.Fatal(err)
+			}
+			args = []string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}
+		}
+		err := Parse(fs, args, tc.envPrefix)
+		if (err != nil) && (tc.wantErr != "") {
+			if !strings.Contains(err.Error(), tc.wantErr) {
+				t.Errorf("expected err contains %q, but got %q", tc.wantErr, err)
+			}
+			return
+		}
+		if (err == nil) && (tc.wantErr != "") {
+			t.Error("expected error but got nil")
+		}
+		if err != nil && (tc.wantErr == "") {
+			t.Error(err)
+		}
+		if tc.wantFlag != "" {
+			if g, w := flags.accessKey, tc.wantFlag; g != w {
+				t.Errorf("got %q, want %q", g, w)
+			}
+		}
+	}
+
+	run(t, testFunc, "", testCase{
+		configEnv: `
+			-access-key=env
+			`,
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		configEnv: `
+			-access-key=env
+			`,
+		wantFlag: "env",
+	})
+	run(t, testFunc, "", testCase{
+		configEnv: `
+			-access-key=env
+			`,
+		config: `
+			-access-key=🔑
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		envPrefix: "PREFIX_",
+		configEnv: `
+			-access-key=env
+			`,
+		config: `
+			-access-key=🔑
+			`,
+		wantFlag: "🔑",
+	})
+}
+
+func TestParseLoadFile(t *testing.T) {
 	tempDir := t.TempDir()
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			fs, flags := newFlagSet()
-			var args []string
-			if tt.configEnv != "" {
-				f, err := os.CreateTemp(tempDir, "")
-				if err != nil {
-					t.Fatal(err)
-				}
-				defer f.Close()
-				_, err = io.Copy(f, strings.NewReader(tt.configEnv))
-				if err != nil {
-					t.Fatal(err)
-				}
-				t.Setenv(tt.envPrefix+strings.ToUpper(configFlagName), f.Name())
-			}
-			if tt.config != "" {
-				f, err := os.CreateTemp(tempDir, "")
-				if err != nil {
-					t.Fatal(err)
-				}
-				defer f.Close()
-				_, err = io.Copy(f, strings.NewReader(tt.config))
-				if err != nil {
-					t.Fatal(err)
-				}
-				args = []string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}
-			}
-			err := Parse(fs, args, tt.envPrefix)
-			if (err != nil) && (tt.wantErr != "") {
-				if !strings.Contains(err.Error(), tt.wantErr) {
-					t.Errorf("expected err contains %q, but got %q", tt.wantErr, err)
-				}
-				return
-			}
-			if (err == nil) && (tt.wantErr != "") {
-				t.Error("expected error but got nil")
-			}
-			if err != nil && (tt.wantErr == "") {
-				t.Error(err)
-			}
-			if tt.wantFlag != "" {
-				if g, w := flags.accessKey, tt.wantFlag; g != w {
-					t.Errorf("got %q, want %q", g, w)
-				}
-			}
-		})
+
+	type testCase struct {
+		config   string
+		wantFlag string
+		wantErr  string
 	}
+
+	testFunc := func(t *testing.T, tc testCase) {
+		fs, flags := newFlagSet()
+		var args []string
+		f, err := os.CreateTemp(tempDir, "")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer f.Close()
+		_, err = io.Copy(f, strings.NewReader(tc.config))
+		if err != nil {
+			t.Fatal(err)
+		}
+		args = []string{fmt.Sprintf("-%s=%s", configFlagName, f.Name())}
+		err = Parse(fs, args, "")
+		if (err != nil) && (tc.wantErr != "") {
+			if !strings.Contains(err.Error(), tc.wantErr) {
+				t.Errorf("expected err contains %q, but got %q", tc.wantErr, err)
+			}
+			return
+		}
+		if (err == nil) && (tc.wantErr != "") {
+			t.Error("expected error but got nil")
+		}
+		if err != nil && (tc.wantErr == "") {
+			t.Error(err)
+		}
+		if tc.wantFlag != "" {
+			if g, w := flags.accessKey, tc.wantFlag; g != w {
+				t.Errorf("got %q, want %q", g, w)
+			}
+		}
+	}
+
+	run(t, testFunc, "", testCase{
+		config: `
+			-access-key=🔑
+			`,
+		wantFlag: "🔑",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			-access-key 🔑 extra
+			`,
+		wantErr: "syntax error",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			ACCESS_KEY =🔑
+			`,
+		wantErr: "syntax error",
+	})
+	run(t, testFunc, "", testCase{
+		config: `
+			ACCESS KEY =🔑
+			`,
+		wantErr: "syntax error",
+	})
 }
